b8f5f1e8a30bc133c49aacf7df7354f44e46946e
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-11-04 22:43:48 +0530

    Auth context and files added amd react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Background from "./Components/Background"
+import Home from "./Components/Home"
 
 function App() {
 
   return (
     <div className="relative w-full h-screen bg-zinc-800">
       <Background/>
+      <Home/>
     </div>
   )
 }
