bfaaaa09e4c0aa6c4c58c011e3f9c7609e7debb2
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-11-05 22:48:28 +0530

    protected routes

--- src/Context/ProtectedRoute.jsx
+++ src/Context/ProtectedRoute.jsx
@@ -1,6 +1,15 @@
 import { Navigate } from "react-router-dom";
 import { useAuth } from "./AuthContext";
 
+//called from App.jsx
+/*
+ProtectedRoute is a guard for routes.
+It checks:
+  Is user logged in?
+  Does user have the correct role (admin/user)?
+  If not, it redirects to the login page.
+  If yes, it allows access.
+*/
 const ProtectedRoute = ({ children, role }) => {
   const { user } = useAuth();
 
@@ -8,7 +17,7 @@ const ProtectedRoute = ({ children, role }) => {
 
   if (role && user.role !== role) return <Navigate to="/" />;
 
-  return children;
+  return children; //here children is the component passed via App.jsx
 };
 
 export default ProtectedRoute;
