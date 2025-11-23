f098acd1dca32865a35756fd5e964e1ff1fc2d31
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-11-23 11:30:31 +0530

    api call setup to update password

--- src/APICalls/userApi.js
+++ src/APICalls/userApi.js
@@ -1,5 +1,5 @@
 //all calls made to /user endpoint
-export const getProfile = async (token,setprofile) => {
+export const getProfile = async (token, setprofile) => {
   try {
     const res = await fetch(
       `${import.meta.env.VITE_API_BASE_URL}/user/profile`,
@@ -17,30 +17,26 @@ export const getProfile = async (token,setprofile) => {
       return;
     }
     const data = await res.json();
-   // console.log("user profile:", data);
-   setprofile(data && data?.userProfile)
-    
+    // console.log("user profile:", data);
+    setprofile(data && data?.userProfile);
   } catch (error) {
     console.log("Failed to user profile", error);
     alert("Failed to user profile");
   }
 };
 
-export const updateProfile= async (dataToUpdate,id, token,setreload) => {
-//   console.log("id to update:",id)
-//   console.log("dataToUpdate:",dataToUpdate)
+export const updateProfile = async (dataToUpdate, id, token, setreload) => {
+  //   console.log("id to update:",id)
+  //   console.log("dataToUpdate:",dataToUpdate)
   try {
-    const res = await fetch(
-      `${import.meta.env.VITE_API_BASE_URL}/user/${id}`,
-      {
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
-        },
-         body:JSON.stringify(dataToUpdate)
-      }
-    );
+    const res = await fetch(`${import.meta.env.VITE_API_BASE_URL}/user/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify(dataToUpdate),
+    });
     if (!res.ok) {
       const err = await res.json();
       alert(err.error || "Failed to update user");
@@ -48,8 +44,7 @@ export const updateProfile= async (dataToUpdate,id, token,setreload) => {
     }
     const data = await res.json();
     console.log("user updated:", data);
-  setreload((prev)=>!prev)
-    
+    setreload((prev) => !prev);
   } catch (error) {
     console.log("Failed to user candidate", error);
     alert("Failed to user candidate");
@@ -58,16 +53,13 @@ export const updateProfile= async (dataToUpdate,id, token,setreload) => {
 
 export const getAllVoters = async (setVoters, token) => {
   try {
-    const res = await fetch(
-      `${import.meta.env.VITE_API_BASE_URL}/user/all`,
-      {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
-        },
-      }
-    );
+    const res = await fetch(`${import.meta.env.VITE_API_BASE_URL}/user/all`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
     if (!res.ok) {
       const err = await res.json();
       alert(err.error || "Failed to fetch voters");
@@ -81,5 +73,36 @@ export const getAllVoters = async (setVoters, token) => {
   } catch (error) {
     console.log("Failed to fetch voters", error);
     alert("Failed to fetch voters");
+  }
+};
+
+export const updatePassword = async (currentPassword, newPassword, token) => {
+    console.log("currentPassword:",.currentPassword)
+    console.log("newPassword:",newPassword)
+  try {
+    const res = await fetch(
+      `${import.meta.env.VITE_API_BASE_URL}/user/profile/password`,
+      {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({
+          currentPassword: currentPassword,
+          newPassword: newPassword,
+        }),
+      }
+    );
+    if (!res.ok) {
+      const err = await res.json();
+      alert(err.error || "Failed to update password");
+      return;
+    }
+    const data = await res.json();
+    console.log("password updated:", data);
+  } catch (error) {
+    console.log("Failed to update password", error);
+    alert("Failed to update password");
   }
 };
